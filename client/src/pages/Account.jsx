2e47fe71c9c2515c5b76709cac4644d9f134e57a
Mariano <longo.mariano93@yahoo.it>
2025-05-06 16:33:26 +0200

    fix

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -9,7 +9,7 @@ function Account() {
   const [scores, setScores] = useState([]);
   const [message, setMessage] = useState("");
   const [showPasswordForm, setShowPasswordForm] = useState(false);
-  const backendUrl = import.meta.env.REACT_APP_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_APP_BACKEND_URL;
   const token = localStorage.getItem("token"); // Assicurati di memorizzare il token JWT
 
   useEffect(() => {

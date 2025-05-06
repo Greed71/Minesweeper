96a909dad95b1949a10bc5d951139de0cd217e36
Mariano <longo.mariano93@yahoo.it>
2025-05-06 17:52:28 +0200

    fix

--- server/src/main/java/com/MinesweeperApplication.java
+++ server/src/main/java/com/MinesweeperApplication.java
@@ -5,17 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class MinesweeperApplication {
 
     public static void main(String[] args) {
-        // Carica il file .env prima di avviare Spring Boot
-        Dotenv dotenv = Dotenv.load();
-        
-        // Ottieni la variabile FRONT dal file .env
-        String frontUrl = dotenv.get("FRONT");
+        // Leggi la variabile d'ambiente FRONT direttamente dal sistema
+        String frontUrl = System.getenv("FRONT");
 
         // Imposta la variabile come proprietà di sistema per usarla in Spring Boot
         System.setProperty("FRONT", frontUrl);
@@ -24,7 +19,7 @@ public class MinesweeperApplication {
         SpringApplication.run(MinesweeperApplication.class, args);
     }
 
-    @CrossOrigin(origins = "${FRONT}", allowCredentials = "true")  // Usa il valore di FRONT nella configurazione di CORS
+    @CrossOrigin(origins = "${FRONT}", allowCredentials = "true")
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
